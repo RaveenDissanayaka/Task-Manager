c2b930588a4be5215513e5bbbf29743ae7de017e
RaveenDissanayaka <raveensalinda50@gmail.com>
2024-08-14 11:44:28 +0530

    Update DBManager

--- backend/DBManager.php
+++ backend/DBManager.php
@@ -1,7 +1,13 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: SHALINDA
- * Date: 2024-08-14
- * Time: 11:21 AM
- */
+// DB credentials.
+define('DB_HOST', 'localhost'); // Host name
+define('DB_USER', 'root'); // user name
+define('DB_PASS', ''); // db user password
+define('DB_NAME', 'task_manager'); // db name
+// Establish database connection.
+try {
+    $dbh = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASS);
+} catch (PDOException $e) {
+    exit("Error: " . $e->getMessage());
+}
+?>
